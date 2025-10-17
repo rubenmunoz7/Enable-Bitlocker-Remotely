b7225e342c0ad06d5190719a059f72536f0a550e
rubenmunoz7 <munozruben324@gmail.com>
2025-10-17 11:57:27 -0400

    Update enable_bitlocker_script.ps1

--- enable_bitlocker_script.ps1
+++ enable_bitlocker_script.ps1
@@ -10,45 +10,43 @@ Write-Host "NINJA_BITLOCKER: START"     # Marker for N1 log
 try {
     $v = Get-BitLockerVolume -MountPoint $os # Grab current Bitlocker state from the OS drive    
 
-}
-
     # Exit is it is already encrypted
-    if ($v -and $v.ProtectionStatus -eq 1){
+    if ($v -and $v.ProtectionStatus -eq 1){ # if drive is already encrypted
         Write-Host "NINJA_BITLOCKER: ALREADY ENCRYPTED" # Output log
         exit 0
     }
 
     # Check TPM status 
     # IF a TPM exists, but isn't ready, then initialize if needed using "Initialize-tpm"
-    $tpm = Get-Tpm -ErrorAction SilentlyContinue
-    $tpmReady = $false
-    if ($tpm -and $tpm.TpmPresent) {
-        if (-not $tpm.TpmReady) {
+    $tpm = Get-Tpm -ErrorAction SilentlyContinue 
+    $tpmReady = $false  # Default flag
+    if ($tpm -and $tpm.TpmPresent) { # if tpm exists
+        if (-not $tpm.TpmReady) { # If TPM is not ready, initialize it
             Initialize-Tpm -ErrorAction SilentlyContinue | Out-Null
-            Start-Sleep 2
+            Start-Sleep 2 # pause for 2 seconds
         }
         # Retry TPM state after initialization attempt
-        $tpm = Get-Tpm -ErrorAction SilentlyContinue
-        $tpmReady = ($tpm -and $tpm.TpmReady)
+        $tpm = Get-Tpm -ErrorAction SilentlyContinue    # Recheck TPM
+        $tpmReady = ($tpm -and $tpm.TpmReady)           # Ready flag
     }
     Write-Host "NINJA_BITLOCKER: TPM_READY=$tpmReady" # Output
 
     if (-not $tpmReady) {
-        Write-Error "NINJA_BITLOCKER: TPM not present/ready"
-        exit 2 # alert
+        Write-Error "NINJA_BITLOCKER: TPM not present/ready" # If TPM is not ready, stop
+        exit 2 # error code
     }
 
     # enable with TPM protector
     Write-Host "NINJA_BLOCKER: ENABLE_ATTEMPT (TPMProtector)"
-    Enable-BitLocker -MountPoint $os -EncryptionMethod XtsAes256 -UsedSpaceOnly -TpmProtector
+    Enable-BitLocker -MountPoint $os -EncryptionMethod XtsAes256 -UsedSpaceOnly -TpmProtector   # Enable Bitlocker on C: drive
 
     Start-Sleep 2
-    $post = Get-BitlockerVolume -mountPoint $os
+    $post = Get-BitlockerVolume -mountPoint $os     # Check post-encrpytion
     Write-Host "NINJA_BITLOCKER: STATUS=$($post.ProtectionStatus)" # 1 = On
-    Write-Host "NINJA_BITLOCKER: ENABLED_OK"
+    Write-Host "NINJA_BITLOCKER: ENABLED_OK"     # Log success
     exit 0
 }
 catch {
-    Write-Error ("NINJA_BITLOCKER: ERROR " + $ .Exception.Message)
+    Write-Error ("NINJA_BITLOCKER: ERROR: " + $_.Exception.Message)         # Write any errors, then exit
     exit 1
 }
